d5c07aabd642d31047a71495d70d5343e312c69a
Merlot44 <stevegaret448@gmail.com>
2025-03-24 09:31:50 -0400

    Started main code Added ressources

--- WNDMain.Designer.vb
+++ WNDMain.Designer.vb
@@ -22,18 +22,82 @@ Partial Class WNDMain
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        PBXSound = New PictureBox()
+        LBLTitle = New Label()
+        BTNInstructions = New Label()
+        BTNStart = New Label()
+        CType(PBXSound, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
+        ' 
+        ' PBXSound
+        ' 
+        PBXSound.BackColor = Color.Transparent
+        PBXSound.Image = My.Resources.Resources.Unmute
+        PBXSound.Location = New Point(882, 12)
+        PBXSound.Name = "PBXSound"
+        PBXSound.Size = New Size(50, 50)
+        PBXSound.SizeMode = PictureBoxSizeMode.Zoom
+        PBXSound.TabIndex = 0
+        PBXSound.TabStop = False
+        ' 
+        ' LBLTitle
+        ' 
+        LBLTitle.BackColor = Color.Transparent
+        LBLTitle.Font = New Font("Viner Hand ITC", 30F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        LBLTitle.ForeColor = Color.White
+        LBLTitle.Location = New Point(27, 12)
+        LBLTitle.Name = "LBLTitle"
+        LBLTitle.Size = New Size(293, 134)
+        LBLTitle.TabIndex = 1
+        LBLTitle.Text = "L’empire arithmétique"
+        LBLTitle.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' BTNInstructions
+        ' 
+        BTNInstructions.BackColor = Color.Transparent
+        BTNInstructions.Font = New Font("Comic Sans MS", 30F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        BTNInstructions.ForeColor = Color.White
+        BTNInstructions.Location = New Point(38, 504)
+        BTNInstructions.Name = "BTNInstructions"
+        BTNInstructions.Size = New Size(268, 113)
+        BTNInstructions.TabIndex = 3
+        BTNInstructions.Text = "Instructions"
+        BTNInstructions.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' BTNStart
+        ' 
+        BTNStart.BackColor = Color.Transparent
+        BTNStart.Font = New Font("Comic Sans MS", 30F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        BTNStart.ForeColor = Color.White
+        BTNStart.Location = New Point(38, 260)
+        BTNStart.Name = "BTNStart"
+        BTNStart.Size = New Size(268, 114)
+        BTNStart.TabIndex = 2
+        BTNStart.Text = "Débuter"
+        BTNStart.TextAlign = ContentAlignment.MiddleCenter
         ' 
         ' WNDMain
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(1008, 729)
+        BackgroundImage = My.Resources.Resources.Page_1
+        BackgroundImageLayout = ImageLayout.Center
+        ClientSize = New Size(944, 681)
+        Controls.Add(BTNInstructions)
+        Controls.Add(LBLTitle)
+        Controls.Add(BTNStart)
+        Controls.Add(PBXSound)
         FormBorderStyle = FormBorderStyle.FixedSingle
         MaximizeBox = False
         Name = "WNDMain"
         Text = "L'empire arithmétique"
+        CType(PBXSound, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
     End Sub
+
+    Friend WithEvents PBXSound As PictureBox
+    Friend WithEvents LBLTitle As Label
+    Friend WithEvents BTNStart As Label
+    Friend WithEvents BTNInstructions As Label
 
 End Class
--- WNDMain.vb
+++ WNDMain.vb
@@ -1,3 +1,28 @@
 ﻿Public Class WNDMain
+    Private Sub BTNStart_Click(sender As Object, e As EventArgs) Handles BTNStart.Click
 
+    End Sub
+
+
+    Private Sub BTNInstructions_Click(sender As Object, e As EventArgs) Handles BTNInstructions.Click
+        LBLTitle.Visible = False
+        BTNInstructions.Visible = False
+        BackgroundImage = My.Resources.Page_2
+        ' BTNStart.Location.Y = (38, 192)
+    End Sub
+
+    Private Sub PBXSound_Click(sender As Object, e As EventArgs) Handles PBXSound.Click
+
+    End Sub
+
+    Private Sub BTNStart_MouseHover(sender As Object, e As EventArgs) Handles BTNStart.MouseHover
+        BTNStart.Cursor = Cursors.Hand
+    End Sub
+    Private Sub BTNInstructions_MouseHover(sender As Object, e As EventArgs) Handles BTNInstructions.MouseHover
+        BTNInstructions.Cursor = Cursors.Hand
+    End Sub
+
+    Private Sub PBXSound_MouseHover(sender As Object, e As EventArgs) Handles PBXSound.MouseHover
+        PBXSound.Cursor = Cursors.Hand
+    End Sub
 End Class
